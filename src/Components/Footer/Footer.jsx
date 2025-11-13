2d9a4862966b94ec4a6d5fe33935d03c78bc8425
Sazzadkhan45 <sazzadkhan753@gmail.com>
2025-11-13 23:47:43 +0600

    Simple footer design done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,54 +1,97 @@
 import React, { use } from "react";
 import MyContainer from "../MyContainer";
 import { ThemeContext } from "../../Providers/ThemeContext";
+import { Link } from "react-router";
+import { FaBehance, FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { MdLocalPhone, MdOutlineMail } from "react-icons/md";
 
 const Footer = () => {
   // Theme context
   const { isDark } = use(ThemeContext);
   return (
-    <div className={`${isDark ? "bg-gray-700" : "bg-base-300"}`}>
+    <div className={` ${isDark ? "bg-gray-700" : "bg-base-300"}`}>
       <MyContainer>
-        <footer className="footer sm:footer-horizontal  text-base-content py-10">
+        <footer className="md:flex justify-between md:gap-8 space-y-8 py-10 px-4 md:px-0">
           <nav>
-            <h6 className="footer-title">Services</h6>
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
+            <h6 className="footer-title border-b-2">Contact Info</h6>
+            <p className="flex  items-center gap-1">
+              <MdLocalPhone /> +8801850899
+            </p>
+            <p className="flex  items-center gap-1">
+              <MdOutlineMail /> sazzad753@gmail.com
+            </p>
+            <div className="flex gap-3 cursor-pointer mt-2 text-black">
+              {/*  */}
+              <Link
+                className="bg-amber-50 p-1 rounded-full"
+                to="https://www.facebook.com/"
+                target="_blank"
+              >
+                <FaFacebookF size={15} />
+              </Link>
+              <Link
+                className="bg-amber-50 p-1 rounded-full"
+                to="https://www.instagram.com/"
+                target="_blank"
+              >
+                <FaInstagram size={15} />
+              </Link>
+              <Link
+                className="bg-amber-50 p-1 rounded-full"
+                to="https://x.com/home"
+                target="_blank"
+              >
+                <FaXTwitter size={15} />
+              </Link>
+              <Link
+                className="bg-amber-50 p-1 rounded-full"
+                to="https://www.facebook.com/"
+                target="_blank"
+              >
+                <FaBehance size={15} />
+              </Link>
+            </div>
           </nav>
           <nav>
-            <h6 className="footer-title">Company</h6>
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
+            <h6 className="footer-title border-b-2">Legal Info</h6>
+            <p className="link link-hover">Terms of use</p>
+            <p className="link link-hover">Privacy policy</p>
+            <p className="link link-hover">Cookie policy</p>
           </nav>
-          <nav>
-            <h6 className="footer-title">Legal</h6>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
-          </nav>
-          <form>
-            <h6 className="footer-title">Newsletter</h6>
-            <fieldset className="w-80">
-              <label>Enter your email address</label>
-              <div className="join">
-                <input
-                  type="text"
-                  placeholder="username@site.com"
-                  className="input input-bordered join-item"
-                />
-                <button className="btn btn-primary join-item">Subscribe</button>
-              </div>
-            </fieldset>
-          </form>
+          <div>
+            <Link
+              to="/"
+              className="text-lg md:text-xl lg:text-2xl font-medium border-b-2"
+            >
+              Artify<span className="font-bold">Nest</span>
+            </Link>
+            <form className="mt-3">
+              <fieldset className="w-80">
+                <label>Enter your email address</label>
+                <div className="join">
+                  <input
+                    type="text"
+                    placeholder="username@site.com"
+                    className="input input-bordered join-item"
+                  />
+                  <button className="btn btn-secondary join-item">
+                    Subscribe
+                  </button>
+                </div>
+              </fieldset>
+            </form>
+          </div>
         </footer>
       </MyContainer>
-      <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+      <footer
+        className={`footer sm:footer-horizontal footer-center  text-base-content p-4 ${
+          isDark ? "bg-gray-800 " : "bg-gray-300 "
+        }`}
+      >
         <aside>
           <p>
-            Copyright © {new Date().getFullYear()} - All right reserved by{" "}
+            Copyright © {new Date().getFullYear()} - All right{" "}
             <span className="font-bold">ArtifyNest</span>
           </p>
         </aside>
